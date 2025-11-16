b7c2f8cbaa5e916b9db274147460b46fd05486a8
doppelsana <sana@doppel.moe>
2025-11-16 05:21:49 -0500

    (modules) removing sana-rebuild alias

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -4,7 +4,6 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      sana-rebuild = "sudo nixos-rebuild switch --flake /home/sana/.nix#PASSENGER";
       la = "ls -la";
       ".." = "cd ..";
       kssh = "kitten ssh";
