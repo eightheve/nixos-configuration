fec719aa47640e69b02797344dc337486c8c2961
doppelsana <sana@doppel.moe>
2025-12-09 03:06:58 -0500

    (satellite) removing get-text-length script

--- modules/home-manager/hyprland/SATELLITE/get-text-length.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-text="$1"
-font="$2"
-
-ffmpeg -v quiet -f lavfi -i color=size=1x1 \
-	-vf "drawtext=fontfile=${font}:text='${text}':x=0:y=0:fontsize=48:print_textinfo=1" \
-	-frames:v 1 -f null - 2>&1 | awk -F'[:, ]+''/text_w/{print $3}'
