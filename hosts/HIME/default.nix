a350035016e5556f6152624e8f81e22cb1ed5a84
doppelsana <sana@doppel.moe>
2025-12-28 10:34:13 -0500

    (hamuko/hime) initial commit

---
+++ hosts/HIME/default.nix
@@ -0,0 +1,37 @@
+{ ... }:
+
+{
+  imports = [
+    ./hardware.nix
+  ];
+
+  boot.loader.grub = {
+    enable = true;
+    device = "CHANGE-THIS";
+  };
+
+  networking = {
+    hostName = "HIME";
+    networkmanager.enable = false;
+
+    interfaces.eno3 = {
+      useDHCP = false;
+      ipv4.addresses = [ {
+        address = "192.168.1.40";
+        prefixLength = 24;
+      } ];
+    };
+
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "8.8.8.8" "1.1.1.1" ];
+  };
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false;
+  networking.firewall.enable = false;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
